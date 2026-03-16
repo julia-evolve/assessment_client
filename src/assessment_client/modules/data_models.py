5166082fbc2d1d92fed214459abe510c19cbce37
Julia Rebrova <julia@evolveplatform.ai>
2026-03-16 19:10:55 +0700

    Reorder import statements in data_models.py

--- src/assessment_client/modules/data_models.py
+++ src/assessment_client/modules/data_models.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 from pydantic import BaseModel, Field, ConfigDict
-from typing import List, Literal, Optional
+from typing import List, Optional, Literal
 
 
 class IndicatorLevel(BaseModel):
